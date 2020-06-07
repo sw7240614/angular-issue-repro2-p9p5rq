840cf39929a38dd42d254333ef894a326d071575
sw7240614 <sw7240514@gmail.com>
2020-06-07 12:01:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-issue-repro2-p9p5rq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-issue-repro2-p9p5rq)
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  // Replace with your markup
+  template: `
+`,
+  styles: [ `h2 { font-weight: normal; }
+  `]
+})
+
+
+export class AppComponent  {
+  // your code goes here
+}
